75122607d3a58e15a81cd3e35692e80585149df4
nurkhairunnisaidrus <knisa981231@gmail.com>
2021-10-22 14:29:17 +0800

    lab2
    lab2

---
+++ workspace-dadlabs/.metadata/.plugins/org.eclipse.core.resources/.history/39/f0e653cc0033001c14dae9709b2ffd4e
@@ -0,0 +1,16 @@
+package lab02.app;
+
+/**
+* This application rounding the number to the nearest decimal points.
+*
+* @author emalianakasmuri
+*
+*/
+
+public class RoundingApplication 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Rounding number to the nearest decimal points");	
+	}
+}
--- workspace-dadlabs/lab02/src/lab02/app/RoundingApplication.java
+++ workspace-dadlabs/lab02/src/lab02/app/RoundingApplication.java
@@ -1,5 +1,7 @@
 package lab02.app;
 
+import lab02.rounding.RoundingManager;
+
 /**
 * This application rounding the number to the nearest decimal points.
 *
@@ -12,5 +14,22 @@ public class RoundingApplication
 	public static void main(String[] args) 
 	{
 		System.out.println("Rounding number to the nearest decimal points");	
+		
+		int divident = 200;
+		int divisor = 357;
+		
+		RoundingManager roundManager = new RoundingManager();
+		double value = roundManager.calculatePercentage(divident, divisor);
+		
+		System.out.println("Progress from " + divisor + "/"
+		+ divident + ": " + value + "%");
+		int decimalPoints = 2;
+		
+		System.out.println("Round to " + decimalPoints + " decimal points: "
+				+ roundManager.roundValue(value, decimalPoints) + "%");
+				decimalPoints = 0;
+				System.out.println("Round to " + decimalPoints + " decimal points: "
+				+ roundManager.roundValue(value, decimalPoints) + "%");
+
 	}
 }
